2232e553d49814e64c8bb1c11b3b1272d5dc05c1
Szymon Sawicki <szymon.sawicki@gmail.com>
2024-10-04 12:31:34 +0200

    foreign_key: { on_delete: :cascade }

--- db/migrate/20220413060517_create_addresses.rb
+++ db/migrate/20220413060517_create_addresses.rb
@@ -6,7 +6,7 @@ class CreateAddresses < ActiveRecord::Migration[7.0]
       t.string :state
       t.integer :zip
       t.string :country
-      t.references :user, foreign_key: true
+      t.references :user, foreign_key: { on_delete: :cascade }
 
       t.timestamps
     end
--- db/migrate/20220427062305_add_category_to_posts.rb
+++ db/migrate/20220427062305_add_category_to_posts.rb
@@ -1,5 +1,5 @@
 class AddCategoryToPosts < ActiveRecord::Migration[7.0]
   def change
-    add_reference :posts, :category, null: false, foreign_key: true
+    add_reference :posts, :category, null: false, foreign_key: { on_delete: :cascade }
   end
 end
